3bad3424a1dfa93b80c9c4bcf67c9986dc92df79
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-06-27 08:53:23 +0530

    feat: initialize Vue 3 project with Vite, add main components and routing

--- calyvim/urls.py
+++ calyvim/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 
 from calyvim.api.accounts.views import AccountViewSet
+from calyvim.views import index
 
 api_router = SimpleRouter(trailing_slash=False)
 
@@ -11,4 +12,5 @@ api_router.register("accounts", AccountViewSet, basename="account")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_router.urls)),
+    re_path(r"^(?:.*)/?$", index, name="index"),  # Catch-all route
 ]
