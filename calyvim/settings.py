55c1a62057bdf79ef6d117e70ac3e2039a6ea70d
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-06-28 20:09:30 +0530

    feat: Add STATIC_ROOT setting in settings.py and remove autogenerated requirements.txt

--- calyvim/settings.py
+++ calyvim/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "dist" / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
